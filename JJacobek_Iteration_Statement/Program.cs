c0365838d4f10186d94e101d710f341f50762b88
Justin Jacobek <jjacobek@usf.edu>
2021-01-28 14:45:54 -0500

    Add project files.

---
+++ JJacobek_Iteration_Statement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace JJacobek_Iteration_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a value between 1 and 100");
+
+            try
+            {
+                String input = Console.ReadLine();
+                int Input_Value = int.Parse(input);
+
+                if (Input_Value > 0 && Input_Value <= 100)
+                {
+                    do
+                    {
+                        Console.WriteLine("you have entered " + Input_Value.ToString() + ". This is the current integer value in the loop: " + Input_Value.ToString());
+                        Input_Value--;
+                    } while (Input_Value > 0);
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey(true);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid integer");
+                }
+            }
+            catch
+            {
+                
+            }
+            
+           
+        }
+    }
+}
